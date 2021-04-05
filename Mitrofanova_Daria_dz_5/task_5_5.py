6dafe0e0fb8fe840f54a62216c4d210bf373bf81
dmitrofanova <dmitrofanova@outlook.com>
2021-04-05 20:08:13 +0300

    fix: homework 5

--- Mitrofanova_Daria_dz_5/task_5_5.py
+++ Mitrofanova_Daria_dz_5/task_5_5.py
@@ -6,12 +6,18 @@
 #
 # Подсказка: напишите сначала решение «в лоб». Потом подумайте об оптимизации.
 
-from collections import OrderedDict
+from collections import Counter
 
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
 # в лоб
 src_distinct = [el for el in src if src.count(el) == 1]
 
+# небольшая оптимизация
+unique_list = list(Counter(src))
+unique_distinct = [el for el in unique_list if src.count(el) == 1]
+
 print(src)
 print(src_distinct)
+print(unique_list)
+print(unique_distinct)
